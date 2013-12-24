10c3fbcf3f6b833fa1055b201f1da690a5634b3a
Konrad <konrad@copper.io>
2013-12-24 13:37:42 +0100

    added DEVISE

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Wow::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://whoowes.herokuapp.com/' }
 end
